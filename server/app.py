a69a37da762c439555cf808343701a32547a28f9
george <gelkik@gmail.com>
2023-03-23 11:19:28 -0400

    final

--- server/app.py
+++ server/app.py
@@ -21,10 +21,105 @@ api = Api(app)
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/restaurants')
+@app.route('/restaurants',methods = ["GET"])
 def restaurants():
+    restaurants = Restaurant.query.all()
 
-    pass
+    if request.method == "GET":
+        restaurants_dict = [restaurants.to_dict() for restaurants in restaurants]
+
+        response = make_response(
+            restaurants_dict,
+            200
+        )
+        return response
+    
+@app.route('/restaurants/<int:id>',methods = ["GET","DELETE"])
+def restaurants_id(id):
+    restaurant = Restaurant.query.filter(Restaurant.id==id).first()
+
+    if not restaurant:
+        response_body = {
+            "error": "Restaurant not found"
+        }
+        response = make_response(
+            response_body,
+            404
+        )
+        return response
+
+    elif request.method == "GET":
+        restaurant_dict = restaurant.to_dict()
+
+        response = make_response(
+            restaurant_dict,
+            200
+        )
+        return response
+
+    elif request.method == "DELETE":
+        try:
+            db.session.delete(restaurant)
+            db.session.commit()
+
+            response = make_response(
+                {},
+                200
+            )
+            return response
+        
+        except:
+            response_body = {
+                "error": "Restaurant not found"
+            }
+
+            response = make_response(
+                response_body,
+                404
+            )
+            return response
+        
+@app.route('/restaurant_pizzas',methods = ['POST'])
+def restaurant_pizzas():
+    # restaurant_pizzas = RestaurantPizza.query.all()
+
+    # if request.method == 'GET':
+    #     restaurant_pizzas_dict = [restaurant_pizzas.to_dict() for restaurant_pizzas in restaurant_pizzas]
+
+    #     response = make_response(
+    #         restaurant_pizzas_dict,
+    #         200
+    #     )
+    #     return response
+    
+    # elif request.method == 'POST':
+    try:
+        new_restaurant = RestaurantPizza(
+            price = request.get_json()['price'],
+            pizza_id = request.get_json()['pizza_id'],
+            restaurant_id = request.get_json()['restaurant_id']
+        )
+        db.session.add(new_restaurant)
+        db.session.commit()
+
+        new_pizza = Pizza.query.filter(Pizza.id == new_restaurant.pizza_id).first()
+        new_dict = new_pizza.to_dict()
+
+        response = make_response(
+            jsonify(new_dict),
+            201
+        )
+
+    except ValueError:
+        response_body = {
+            "errors": ["validation errors"]
+        }
+        response = make_response(
+            response_body,
+            400
+        )
+    return response
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,4 +10,48 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Pizza(db.Model,SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    serialize_rules = ('-restaurant_pizzas.pizza','-restaurants')
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza',backref = 'pizza')
+    restaurants = association_proxy('restaurant_pizzas','restaurant')
+
+class Restaurant(db.Model,SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    serialize_rules = ('-restaurant_pizzas.restaurant','-pizzas')
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship('RestaurantPizza',backref='restaurant')
+    pizzas = association_proxy('restaurant_pizzas','pizza')
+
+class RestaurantPizza(db.Model,SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    serialize_rules = ('-pizza.restuarant_pizzas','-restaurant.restaurant_pizzas')
+
+    id = db.Column(db.Integer,primary_key = True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    
+    @validates('price')
+    def validate_price(self,key,price):
+        if price <1 or price >30:
+            raise ValueError('Price must be between 1 and 30.')
+        return price
+
